6e8700e8f3841c9c0a61dba283bb163db5f7e3b6
jetty <dlwnsdud980727@gmail.com>
2025-10-28 23:09:17 +0900

    [리트코드 687 / 이진트리] 가장 긴 동일 값의 경로

--- leetcode/Tree/LongestUnivaluePath.py
+++ leetcode/Tree/LongestUnivaluePath.py
@@ -7,22 +7,27 @@ class TreeNode:
 def longestUniValue(root: TreeNode):
     longest = [0]
 
-    def dfs(node: TreeNode, k):
-        longest[0] = max(longest[0], k)
+    def dfs(node: TreeNode):
+        if not node:
+            return 0
         
-        if node.left:
-            if node.val == node.left.val:
-                dfs(node.left, k+1)
-            else:
-                dfs(node.left, 1)
+        left = dfs(node.left)
+        right = dfs(node.right)
 
-        if node.right:
-            if node.val == node.right.val:
-                dfs(node.right, k+1)
-            else:
-                dfs(node.right, 1)
+        if node.left and node.val == node.left.val:
+            left += 1
+        else:
+            left = 0
 
-    dfs(root, 1)
+        if node.right and node.val == node.right.val:
+            right += 1
+        else:
+            right = 0
+
+        longest[0] = max(longest[0], left+right)
+        return max(left, right)
+
+    dfs(root)
     return longest[0]
 
 root = TreeNode(1)
